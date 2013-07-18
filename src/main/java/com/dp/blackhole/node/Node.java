34d8ed2fe0602fe3d977b8f83322d87bdb4e9b1e
Erik.fang <fmerik@gmail.com>
2013-07-18 17:47:08 +0800

    catch IOException in Node.loop()

--- src/main/java/com/dp/blackhole/node/Node.java
+++ src/main/java/com/dp/blackhole/node/Node.java
@@ -8,17 +8,12 @@ import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.dp.blackhole.common.AppRollPB.AppRoll;
-import com.dp.blackhole.common.AssignCollectorPB.AssignCollector;
-import com.dp.blackhole.common.MessagePB;
-import com.dp.blackhole.common.AppRegPB.AppReg;
 import com.dp.blackhole.common.MessagePB.Message;
 
 public abstract class Node {
@@ -33,84 +28,90 @@ public abstract class Node {
 	
 	private ConcurrentLinkedQueue<Message> queue; 
 	
-	protected void loop() throws IOException {
+	protected void loop() {
+		SelectionKey key = null;
 		while (running) {
-			selector.select(500);
-			Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
-			while (iter.hasNext()) {
-				SelectionKey key = iter.next();
-				iter.remove();
-				if (key.isConnectable()) {
-					SocketChannel channel = (SocketChannel) key.channel();
-					key.interestOps(key.interestOps() | SelectionKey.OP_READ);
-					channel.finishConnect();
-				} else if (key.isWritable()) {
-					key.interestOps(key.interestOps() & ~SelectionKey.OP_WRITE);
-					SocketChannel channel = (SocketChannel) key.channel();
-					while (true) {
-						if (writebuffer == null) {
-							Message msg = queue.peek();
-							if (msg == null) {
-								break;
+			try {
+				selector.select(500);
+				Iterator<SelectionKey> iter = selector.selectedKeys()
+						.iterator();
+				while (iter.hasNext()) {
+					key = iter.next();
+					iter.remove();
+					if (key.isConnectable()) {
+						SocketChannel channel = (SocketChannel) key.channel();
+						key.interestOps(key.interestOps() | SelectionKey.OP_READ);
+						channel.finishConnect();
+					} else if (key.isWritable()) {
+						key.interestOps(key.interestOps() & ~SelectionKey.OP_WRITE);
+						SocketChannel channel = (SocketChannel) key.channel();
+						while (true) {
+							if (writebuffer == null) {
+								Message msg = queue.peek();
+								if (msg == null) {
+									break;
+								}
+								byte[] array = msg.toByteArray();
+								writebuffer = ByteBuffer.allocate(4 + array.length);
+								writebuffer.putInt(array.length);
+								writebuffer.put(array);
+								writebuffer.flip();
 							}
-							byte[] array = msg.toByteArray();
-							writebuffer = ByteBuffer.allocate(4 + array.length);
-							writebuffer.putInt(array.length);
-							writebuffer.put(array);
-							writebuffer.flip();
-						}
-						if (writebuffer.remaining() == 0) {
-							queue.poll();
-							writebuffer = null;
-							continue;
-						}
-						int num = -1;
-						for (int i = 0; i < 16; i++) {
-							System.out.println(i);
-							System.out.println(writebuffer);
-							num = channel.write(writebuffer);
-							if (num != 0) {
-								break;
+							if (writebuffer.remaining() == 0) {
+								queue.poll();
+								writebuffer = null;
+								continue;
+							}
+							int num = -1;
+							for (int i = 0; i < 16; i++) {
+								System.out.println(i);
+								System.out.println(writebuffer);
+								num = channel.write(writebuffer);
+								if (num != 0) {
+									break;
+								}
+							}
+							if (num == 0) {
+								key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
 							}
 						}
-						if (num == 0) {
-							key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
+					} else if (key.isReadable()) {
+						LOG.debug("read data");
+						SocketChannel channel = (SocketChannel) key.channel();
+
+						int count;
+
+						if (readLength.hasRemaining()) {
+							count = channel.read(readLength);
+							if (count < 0) {
+								closeconnection(key);
+								running = false;
+								break;
+							} else if (readLength.hasRemaining()) {
+								continue;
+							} else {
+								readLength.flip();
+								int length = readLength.getInt();
+								readbuffer = ByteBuffer.allocate(length);
+							}
 						}
-					}	
-				}else if (key.isReadable()) {
-					LOG.debug("read data");
-					SocketChannel channel = (SocketChannel) key.channel();
-					
-					int count;
-					
-					if (readLength.hasRemaining()) {
-						count = channel.read(readLength);
-						if (count <0) {
+
+						count = channel.read(readbuffer);
+						if (count < 0) {
 							closeconnection(key);
-							running = false;
 							break;
-						} else if (readLength.hasRemaining()) {
-							continue;
-						} else {
-							readLength.flip();
-							int length = readLength.getInt();
-							readbuffer = ByteBuffer.allocate(length);							
 						}
-					}
-					
-					count = channel.read(readbuffer);
-					if (count <0) {
-						closeconnection(key);
-						break;
-					}
-					if (readbuffer.remaining() == 0) {
-						readbuffer.flip();
-						Message msg = Message.parseFrom(readbuffer.array());
-						process(msg);
-						readbuffer = null;
-						readLength.clear();
+						if (readbuffer.remaining() == 0) {
+							readbuffer.flip();
+							Message msg = Message.parseFrom(readbuffer.array());
+							process(msg);
+							readbuffer = null;
+							readLength.clear();
+						}
 					}
 				}
+			} catch (IOException e) {
+				closeconnection(key);
 			}
 		}
 	}
