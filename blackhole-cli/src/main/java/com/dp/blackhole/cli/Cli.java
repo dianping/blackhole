e38d3154b2adf74633f11131b0069133499103dd
LantaoJin <lantao.jin@dianping.com>
2014-04-21 19:40:47 +0800

    add synchronized in registerLogReader()

--- blackhole-cli/src/main/java/com/dp/blackhole/cli/Cli.java
+++ blackhole-cli/src/main/java/com/dp/blackhole/cli/Cli.java
@@ -1,9 +1,7 @@
 package com.dp.blackhole.cli;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
@@ -222,7 +220,7 @@ public class Cli {
 
     private void start() throws FileNotFoundException, IOException {
         Properties prop = new Properties();
-        prop.load(new FileReader(new File("config.properties")));
+        prop.load(ClassLoader.getSystemResourceAsStream("config.properties"));
         
         processor = new CliProcessor();
         client = new GenClient<ByteBuffer, SimpleConnection, Cli.CliProcessor>(
