999c05178061a061bcac33535094c28fd920529d
LantaoJin <lantao.jin@dianping.com>
2014-07-10 10:33:46 +0800

    check the last modify time to avoid always reloading the conf file

--- blackhole-broker/src/main/java/com/dp/blackhole/broker/ftp/FTPConfigrationLoader.java
+++ blackhole-broker/src/main/java/com/dp/blackhole/broker/ftp/FTPConfigrationLoader.java
@@ -1,5 +1,7 @@
 package com.dp.blackhole.broker.ftp;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,10 +16,12 @@ public class FTPConfigrationLoader implements Runnable {
     
     private long interval;
     private boolean running;
+    private long lastModify;
     
     public FTPConfigrationLoader(long interval) {
         this.interval = interval;
         this.running = true;
+        this.lastModify = 0L;
     }
     
     public long getInterval() {
@@ -51,22 +55,27 @@ public class FTPConfigrationLoader implements Runnable {
     }
     
     private void reloadConfigration(Properties prop) throws IOException {
-        prop.load(ClassLoader.getSystemResourceAsStream("ftp.properties"));
-        String url = prop.getProperty("ftp.url");
-        int port = Integer.parseInt(prop.getProperty("ftp.port"));
-        String username = prop.getProperty("ftp.username");
-        String password = prop.getProperty("ftp.password");
-        String rootDir = prop.getProperty("ftp.rootdir");
-        FTPConfigration configration = new FTPConfigration(url, port, username, password, rootDir);
-        String ftpTopicStr = prop.getProperty("ftp.topic");
-        if (ftpTopicStr == null) {
-            return;
-        }
-        String[] ftpTopics = ftpTopicStr.split(",");
-        synchronized (transferTopics) {
-            transferTopics.clear();
-            for (int i = 0; i < ftpTopics.length; i++) {
-                transferTopics.put(ftpTopics[i], configration);
+        File confFile = new File(getClass().getClassLoader().getResource("ftp.properties").getFile());
+        long lastModify = confFile.lastModified();
+        if (this.lastModify != lastModify) {
+            this.lastModify = lastModify;
+            prop.load(new FileReader(confFile));
+            String url = prop.getProperty("ftp.url");
+            int port = Integer.parseInt(prop.getProperty("ftp.port"));
+            String username = prop.getProperty("ftp.username");
+            String password = prop.getProperty("ftp.password");
+            String rootDir = prop.getProperty("ftp.rootdir");
+            FTPConfigration configration = new FTPConfigration(url, port, username, password, rootDir);
+            String ftpTopicStr = prop.getProperty("ftp.topic");
+            if (ftpTopicStr == null) {
+                return;
+            }
+            String[] ftpTopics = ftpTopicStr.split(",");
+            synchronized (transferTopics) {
+                transferTopics.clear();
+                for (int i = 0; i < ftpTopics.length; i++) {
+                    transferTopics.put(ftpTopics[i], configration);
+                }
             }
         }
     }
