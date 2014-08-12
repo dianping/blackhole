0ec031dda9669092c9c30ca5f45fc8e143fbca93
LantaoJin <lantao.jin@dianping.com>
2014-08-12 20:44:04 +0800

    committed offset enable

--- blackhole-common/src/main/java/com/dp/blackhole/protocol/control/MessagePB.java
+++ blackhole-common/src/main/java/com/dp/blackhole/protocol/control/MessagePB.java
@@ -71,6 +71,7 @@ public final class MessagePB {
       MAKR_UNRECOVERABLE(31, 32),
       LISTIDLE(32, 33),
       RESTART(33, 34),
+      DUMP_CONSUMER_GROUP(34, 38),
       ;
       
       
@@ -112,6 +113,7 @@ public final class MessagePB {
           case 32: return MAKR_UNRECOVERABLE;
           case 33: return LISTIDLE;
           case 34: return RESTART;
+          case 38: return DUMP_CONSUMER_GROUP;
           default: return null;
         }
       }
@@ -142,7 +144,7 @@ public final class MessagePB {
       }
       
       private static final MessageType[] VALUES = {
-        HEARTBEART, APP_REG, BROKER_REG, ASSIGN_BROKER, READY_BROKER, APP_ROLL, UPLOAD_ROLL, UPLOAD_SUCCESS, UPLOAD_FAIL, RECOVERY_ROLL, RECOVERY_SUCCESS, RECOVERY_FAIL, FAILURE, NOAVAILABLENODE, UNRECOVERABLE, MANUAL_RECOVERY_ROLL, DUMPSTAT, RETIRESTREAM, CONF_REQ, CONF_RES, NOAVAILABLECONF, DUMPCONF, DUMPREPLY, LISTAPPS, REMOVE_CONF, DUMP_APP, CONSUMER_REG, ASSIGN_CONSUMER, OFFSET_COMMIT, TOPICREPORT, CONSUMERREGFAIL, MAKR_UNRECOVERABLE, LISTIDLE, RESTART, 
+        HEARTBEART, APP_REG, BROKER_REG, ASSIGN_BROKER, READY_BROKER, APP_ROLL, UPLOAD_ROLL, UPLOAD_SUCCESS, UPLOAD_FAIL, RECOVERY_ROLL, RECOVERY_SUCCESS, RECOVERY_FAIL, FAILURE, NOAVAILABLENODE, UNRECOVERABLE, MANUAL_RECOVERY_ROLL, DUMPSTAT, RETIRESTREAM, CONF_REQ, CONF_RES, NOAVAILABLECONF, DUMPCONF, DUMPREPLY, LISTAPPS, REMOVE_CONF, DUMP_APP, CONSUMER_REG, ASSIGN_CONSUMER, OFFSET_COMMIT, TOPICREPORT, CONSUMERREGFAIL, MAKR_UNRECOVERABLE, LISTIDLE, RESTART, DUMP_CONSUMER_GROUP, 
       };
       public static MessageType valueOf(
           com.google.protobuf.Descriptors.EnumValueDescriptor desc) {
@@ -306,6 +308,13 @@ public final class MessagePB {
     public boolean hasRestart() { return hasRestart; }
     public com.dp.blackhole.protocol.control.RestartPB.Restart getRestart() { return restart_; }
     
+    // optional .blackhole.DumpConsumerGroup dumpConsumerGroup = 24;
+    public static final int DUMPCONSUMERGROUP_FIELD_NUMBER = 24;
+    private boolean hasDumpConsumerGroup;
+    private com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup dumpConsumerGroup_;
+    public boolean hasDumpConsumerGroup() { return hasDumpConsumerGroup; }
+    public com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup getDumpConsumerGroup() { return dumpConsumerGroup_; }
+    
     private void initFields() {
       type_ = com.dp.blackhole.protocol.control.MessagePB.Message.MessageType.HEARTBEART;
       appReg_ = com.dp.blackhole.protocol.control.AppRegPB.AppReg.getDefaultInstance();
@@ -327,6 +336,7 @@ public final class MessagePB {
       offsetCommit_ = com.dp.blackhole.protocol.control.OffsetCommitPB.OffsetCommit.getDefaultInstance();
       topicReport_ = com.dp.blackhole.protocol.control.TopicReportPB.TopicReport.getDefaultInstance();
       restart_ = com.dp.blackhole.protocol.control.RestartPB.Restart.getDefaultInstance();
+      dumpConsumerGroup_ = com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup.getDefaultInstance();
     }
     public final boolean isInitialized() {
       if (!hasType) return false;
@@ -383,6 +393,9 @@ public final class MessagePB {
       }
       if (hasTopicReport()) {
         if (!getTopicReport().isInitialized()) return false;
+      }
+      if (hasDumpConsumerGroup()) {
+        if (!getDumpConsumerGroup().isInitialized()) return false;
       }
       return true;
     }
@@ -449,6 +462,9 @@ public final class MessagePB {
       }
       if (hasRestart()) {
         output.writeMessage(20, getRestart());
+      }
+      if (hasDumpConsumerGroup()) {
+        output.writeMessage(24, getDumpConsumerGroup());
       }
       getUnknownFields().writeTo(output);
     }
@@ -538,6 +554,10 @@ public final class MessagePB {
       if (hasRestart()) {
         size += com.google.protobuf.CodedOutputStream
           .computeMessageSize(20, getRestart());
+      }
+      if (hasDumpConsumerGroup()) {
+        size += com.google.protobuf.CodedOutputStream
+          .computeMessageSize(24, getDumpConsumerGroup());
       }
       size += getUnknownFields().getSerializedSize();
       memoizedSerializedSize = size;
@@ -757,6 +777,9 @@ public final class MessagePB {
         if (other.hasRestart()) {
           mergeRestart(other.getRestart());
         }
+        if (other.hasDumpConsumerGroup()) {
+          mergeDumpConsumerGroup(other.getDumpConsumerGroup());
+        }
         this.mergeUnknownFields(other.getUnknownFields());
         return this;
       }
@@ -961,6 +984,15 @@ public final class MessagePB {
               }
               input.readMessage(subBuilder, extensionRegistry);
               setRestart(subBuilder.buildPartial());
+              break;
+            }
+            case 194: {
+              com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup.Builder subBuilder = com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup.newBuilder();
+              if (hasDumpConsumerGroup()) {
+                subBuilder.mergeFrom(getDumpConsumerGroup());
+              }
+              input.readMessage(subBuilder, extensionRegistry);
+              setDumpConsumerGroup(subBuilder.buildPartial());
               break;
             }
           }
@@ -1692,6 +1724,43 @@ public final class MessagePB {
         return this;
       }
       
+      // optional .blackhole.DumpConsumerGroup dumpConsumerGroup = 24;
+      public boolean hasDumpConsumerGroup() {
+        return result.hasDumpConsumerGroup();
+      }
+      public com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup getDumpConsumerGroup() {
+        return result.getDumpConsumerGroup();
+      }
+      public Builder setDumpConsumerGroup(com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup value) {
+        if (value == null) {
+          throw new NullPointerException();
+        }
+        result.hasDumpConsumerGroup = true;
+        result.dumpConsumerGroup_ = value;
+        return this;
+      }
+      public Builder setDumpConsumerGroup(com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup.Builder builderForValue) {
+        result.hasDumpConsumerGroup = true;
+        result.dumpConsumerGroup_ = builderForValue.build();
+        return this;
+      }
+      public Builder mergeDumpConsumerGroup(com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup value) {
+        if (result.hasDumpConsumerGroup() &&
+            result.dumpConsumerGroup_ != com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup.getDefaultInstance()) {
+          result.dumpConsumerGroup_ =
+            com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup.newBuilder(result.dumpConsumerGroup_).mergeFrom(value).buildPartial();
+        } else {
+          result.dumpConsumerGroup_ = value;
+        }
+        result.hasDumpConsumerGroup = true;
+        return this;
+      }
+      public Builder clearDumpConsumerGroup() {
+        result.hasDumpConsumerGroup = false;
+        result.dumpConsumerGroup_ = com.dp.blackhole.protocol.control.DumpConsumerGroupPB.DumpConsumerGroup.getDefaultInstance();
+        return this;
+      }
+      
       // @@protoc_insertion_point(builder_scope:blackhole.Message)
     }
     
@@ -1726,45 +1795,48 @@ public final class MessagePB {
       "umpReply.proto\032\020RemoveConf.proto\032\017Broker" +
       "Reg.proto\032\rDumpApp.proto\032\021ConsumerReg.pr" +
       "oto\032\024AssignConsumer.proto\032\022OffsetCommit." +
-      "proto\032\021TopicReport.proto\032\rRestart.proto\"" +
-      "\265\013\n\007Message\022,\n\004type\030\001 \002(\0162\036.blackhole.Me",
-      "ssage.MessageType\022!\n\006appReg\030\002 \001(\0132\021.blac" +
-      "khole.AppReg\022-\n\014assignBroker\030\003 \001(\0132\027.bla" +
-      "ckhole.AssignBroker\022#\n\007appRoll\030\004 \001(\0132\022.b" +
-      "lackhole.AppRoll\022-\n\014recoveryRoll\030\005 \001(\0132\027" +
-      ".blackhole.RecoveryRoll\022+\n\013readyBroker\030\006" +
-      " \001(\0132\026.blackhole.ReadyBroker\022!\n\006rollID\030\007" +
-      " \001(\0132\021.blackhole.RollID\022#\n\007failure\030\010 \001(\013" +
-      "2\022.blackhole.Failure\0223\n\017noAvailableNode\030" +
-      "\t \001(\0132\032.blackhole.NoAvailableNode\022%\n\010str" +
-      "eamId\030\n \001(\0132\023.blackhole.StreamID\022#\n\007conf",
-      "Res\030\013 \001(\0132\022.blackhole.ConfRes\022\'\n\tdumpRep" +
-      "ly\030\014 \001(\0132\024.blackhole.DumpReply\022)\n\nremove" +
-      "Conf\030\r \001(\0132\025.blackhole.RemoveConf\022\'\n\tbro" +
-      "kerReg\030\016 \001(\0132\024.blackhole.BrokerReg\022#\n\007du" +
-      "mpApp\030\017 \001(\0132\022.blackhole.DumpApp\022+\n\013consu" +
-      "merReg\030\020 \001(\0132\026.blackhole.ConsumerReg\0221\n\016" +
-      "assignConsumer\030\021 \001(\0132\031.blackhole.AssignC" +
-      "onsumer\022-\n\014offsetCommit\030\022 \001(\0132\027.blackhol" +
-      "e.OffsetCommit\022+\n\013topicReport\030\023 \001(\0132\026.bl" +
-      "ackhole.TopicReport\022#\n\007restart\030\024 \001(\0132\022.b",
-      "lackhole.Restart\"\334\004\n\013MessageType\022\016\n\nHEAR" +
-      "TBEART\020\001\022\013\n\007APP_REG\020\002\022\016\n\nBROKER_REG\020\003\022\021\n" +
-      "\rASSIGN_BROKER\020\004\022\020\n\014READY_BROKER\020\005\022\014\n\010AP" +
-      "P_ROLL\020\006\022\017\n\013UPLOAD_ROLL\020\007\022\022\n\016UPLOAD_SUCC" +
-      "ESS\020\010\022\017\n\013UPLOAD_FAIL\020\t\022\021\n\rRECOVERY_ROLL\020" +
-      "\n\022\024\n\020RECOVERY_SUCCESS\020\013\022\021\n\rRECOVERY_FAIL" +
-      "\020\014\022\013\n\007FAILURE\020\r\022\023\n\017NOAVAILABLENODE\020\016\022\021\n\r" +
-      "UNRECOVERABLE\020\017\022\030\n\024MANUAL_RECOVERY_ROLL\020" +
-      "\020\022\014\n\010DUMPSTAT\020\021\022\020\n\014RETIRESTREAM\020\022\022\014\n\010CON" +
-      "F_REQ\020\023\022\014\n\010CONF_RES\020\024\022\023\n\017NOAVAILABLECONF",
-      "\020\025\022\014\n\010DUMPCONF\020\026\022\r\n\tDUMPREPLY\020\027\022\014\n\010LISTA" +
-      "PPS\020\030\022\017\n\013REMOVE_CONF\020\031\022\014\n\010DUMP_APP\020\032\022\020\n\014" +
-      "CONSUMER_REG\020\033\022\023\n\017ASSIGN_CONSUMER\020\034\022\021\n\rO" +
-      "FFSET_COMMIT\020\035\022\017\n\013TOPICREPORT\020\036\022\023\n\017CONSU" +
-      "MERREGFAIL\020\037\022\026\n\022MAKR_UNRECOVERABLE\020 \022\014\n\010" +
-      "LISTIDLE\020!\022\013\n\007RESTART\020\"B.\n!com.dp.blackh" +
-      "ole.protocol.controlB\tMessagePB"
+      "proto\032\021TopicReport.proto\032\rRestart.proto\032" +
+      "\027DumpConsumerGroup.proto\"\207\014\n\007Message\022,\n\004",
+      "type\030\001 \002(\0162\036.blackhole.Message.MessageTy" +
+      "pe\022!\n\006appReg\030\002 \001(\0132\021.blackhole.AppReg\022-\n" +
+      "\014assignBroker\030\003 \001(\0132\027.blackhole.AssignBr" +
+      "oker\022#\n\007appRoll\030\004 \001(\0132\022.blackhole.AppRol" +
+      "l\022-\n\014recoveryRoll\030\005 \001(\0132\027.blackhole.Reco" +
+      "veryRoll\022+\n\013readyBroker\030\006 \001(\0132\026.blackhol" +
+      "e.ReadyBroker\022!\n\006rollID\030\007 \001(\0132\021.blackhol" +
+      "e.RollID\022#\n\007failure\030\010 \001(\0132\022.blackhole.Fa" +
+      "ilure\0223\n\017noAvailableNode\030\t \001(\0132\032.blackho" +
+      "le.NoAvailableNode\022%\n\010streamId\030\n \001(\0132\023.b",
+      "lackhole.StreamID\022#\n\007confRes\030\013 \001(\0132\022.bla" +
+      "ckhole.ConfRes\022\'\n\tdumpReply\030\014 \001(\0132\024.blac" +
+      "khole.DumpReply\022)\n\nremoveConf\030\r \001(\0132\025.bl" +
+      "ackhole.RemoveConf\022\'\n\tbrokerReg\030\016 \001(\0132\024." +
+      "blackhole.BrokerReg\022#\n\007dumpApp\030\017 \001(\0132\022.b" +
+      "lackhole.DumpApp\022+\n\013consumerReg\030\020 \001(\0132\026." +
+      "blackhole.ConsumerReg\0221\n\016assignConsumer\030" +
+      "\021 \001(\0132\031.blackhole.AssignConsumer\022-\n\014offs" +
+      "etCommit\030\022 \001(\0132\027.blackhole.OffsetCommit\022" +
+      "+\n\013topicReport\030\023 \001(\0132\026.blackhole.TopicRe",
+      "port\022#\n\007restart\030\024 \001(\0132\022.blackhole.Restar" +
+      "t\0227\n\021dumpConsumerGroup\030\030 \001(\0132\034.blackhole" +
+      ".DumpConsumerGroup\"\365\004\n\013MessageType\022\016\n\nHE" +
+      "ARTBEART\020\001\022\013\n\007APP_REG\020\002\022\016\n\nBROKER_REG\020\003\022" +
+      "\021\n\rASSIGN_BROKER\020\004\022\020\n\014READY_BROKER\020\005\022\014\n\010" +
+      "APP_ROLL\020\006\022\017\n\013UPLOAD_ROLL\020\007\022\022\n\016UPLOAD_SU" +
+      "CCESS\020\010\022\017\n\013UPLOAD_FAIL\020\t\022\021\n\rRECOVERY_ROL" +
+      "L\020\n\022\024\n\020RECOVERY_SUCCESS\020\013\022\021\n\rRECOVERY_FA" +
+      "IL\020\014\022\013\n\007FAILURE\020\r\022\023\n\017NOAVAILABLENODE\020\016\022\021" +
+      "\n\rUNRECOVERABLE\020\017\022\030\n\024MANUAL_RECOVERY_ROL",
+      "L\020\020\022\014\n\010DUMPSTAT\020\021\022\020\n\014RETIRESTREAM\020\022\022\014\n\010C" +
+      "ONF_REQ\020\023\022\014\n\010CONF_RES\020\024\022\023\n\017NOAVAILABLECO" +
+      "NF\020\025\022\014\n\010DUMPCONF\020\026\022\r\n\tDUMPREPLY\020\027\022\014\n\010LIS" +
+      "TAPPS\020\030\022\017\n\013REMOVE_CONF\020\031\022\014\n\010DUMP_APP\020\032\022\020" +
+      "\n\014CONSUMER_REG\020\033\022\023\n\017ASSIGN_CONSUMER\020\034\022\021\n" +
+      "\rOFFSET_COMMIT\020\035\022\017\n\013TOPICREPORT\020\036\022\023\n\017CON" +
+      "SUMERREGFAIL\020\037\022\026\n\022MAKR_UNRECOVERABLE\020 \022\014" +
+      "\n\010LISTIDLE\020!\022\013\n\007RESTART\020\"\022\027\n\023DUMP_CONSUM" +
+      "ER_GROUP\020&B.\n!com.dp.blackhole.protocol." +
+      "controlB\tMessagePB"
     };
     com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner assigner =
       new com.google.protobuf.Descriptors.FileDescriptor.InternalDescriptorAssigner() {
@@ -1776,7 +1848,7 @@ public final class MessagePB {
           internal_static_blackhole_Message_fieldAccessorTable = new
             com.google.protobuf.GeneratedMessage.FieldAccessorTable(
               internal_static_blackhole_Message_descriptor,
-              new java.lang.String[] { "Type", "AppReg", "AssignBroker", "AppRoll", "RecoveryRoll", "ReadyBroker", "RollID", "Failure", "NoAvailableNode", "StreamId", "ConfRes", "DumpReply", "RemoveConf", "BrokerReg", "DumpApp", "ConsumerReg", "AssignConsumer", "OffsetCommit", "TopicReport", "Restart", },
+              new java.lang.String[] { "Type", "AppReg", "AssignBroker", "AppRoll", "RecoveryRoll", "ReadyBroker", "RollID", "Failure", "NoAvailableNode", "StreamId", "ConfRes", "DumpReply", "RemoveConf", "BrokerReg", "DumpApp", "ConsumerReg", "AssignConsumer", "OffsetCommit", "TopicReport", "Restart", "DumpConsumerGroup", },
               com.dp.blackhole.protocol.control.MessagePB.Message.class,
               com.dp.blackhole.protocol.control.MessagePB.Message.Builder.class);
           return null;
@@ -1804,6 +1876,7 @@ public final class MessagePB {
           com.dp.blackhole.protocol.control.OffsetCommitPB.getDescriptor(),
           com.dp.blackhole.protocol.control.TopicReportPB.getDescriptor(),
           com.dp.blackhole.protocol.control.RestartPB.getDescriptor(),
+          com.dp.blackhole.protocol.control.DumpConsumerGroupPB.getDescriptor(),
         }, assigner);
   }
   
--- blackhole-supervisor/src/main/java/com/dp/blackhole/supervisor/ConsumerGroupDesc.java
+++ blackhole-supervisor/src/main/java/com/dp/blackhole/supervisor/ConsumerGroupDesc.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -11,16 +12,16 @@ import org.apache.commons.logging.LogFactory;
 public class ConsumerGroupDesc {
     private ConsumerGroup group;
     // currrently available partitions
-    private Map<String, PartitionInfo> partitions;
+    private Map<String, AtomicLong> commitedOffsetMap;
     private Map<ConsumerDesc, ArrayList<String>> consumeMap;
     
     public static final Log LOG = LogFactory.getLog(Supervisor.class);
     
     public ConsumerGroupDesc(ConsumerGroup group, Collection<PartitionInfo> pinfos) {
         this.group = group;
-        partitions = new ConcurrentHashMap<String, PartitionInfo>();
+        commitedOffsetMap = new ConcurrentHashMap<String, AtomicLong>();
         for (PartitionInfo pinfo: pinfos) {
-            partitions.put(pinfo.getId(), new PartitionInfo(pinfo));
+            commitedOffsetMap.put(pinfo.getId(), new AtomicLong(0));
         }
         consumeMap = new ConcurrentHashMap<ConsumerDesc, ArrayList<String>>();
     }
@@ -29,8 +30,8 @@ public class ConsumerGroupDesc {
         return consumeMap.containsKey(consumer);
     }
 
-    public Map<String, PartitionInfo> getPartitions () {
-        return partitions;
+    public Map<String, AtomicLong> getCommitedOffsets () {
+        return commitedOffsetMap;
     }
     
     public Map<ConsumerDesc, ArrayList<String>> getConsumeMap () {
@@ -46,18 +47,11 @@ public class ConsumerGroupDesc {
     }
 
     public void updateOffset(String consumerId, String topic, String partition, long offset) {
-        PartitionInfo info = partitions.get(partition);
-        if (info == null) {
+        AtomicLong commitedOffset = commitedOffsetMap.get(partition);
+        if (commitedOffset == null) {
             LOG.error("can not find PartitionInfo by partition: " + "[" + topic +"]" + partition + " ,request from " + consumerId);
         } else {
-            info.setEndOffset(offset);
+            commitedOffset.set(offset);
         }
     }
-    
-    // TODO
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-    
 }
