2be4114f39ecca0af19e748c5abb1226a70d5c5b
LantaoJin <lantao.jin@dianping.com>
2015-03-10 10:14:50 +0800

    revert unfinished refactor of CheckDone

--- blackhole-check/src/main/java/com/dp/blackhole/check/CheckDone.java
+++ blackhole-check/src/main/java/com/dp/blackhole/check/CheckDone.java
@@ -2,20 +2,15 @@ package com.dp.blackhole.check;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -28,9 +23,6 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.security.SecurityUtil;
 import org.apache.hadoop.security.UserGroupInformation;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import com.dianping.lion.EnvZooKeeperConfig;
 import com.dianping.lion.client.ConfigCache;
@@ -57,7 +49,6 @@ public class CheckDone implements Runnable{
         }
         //reload sources
         List<String> sources = lionConfChange.getAppToHosts().get(ident.app);
-        
         if (sources == null || sources.isEmpty()) {
             LOG.error("source hosts are all miss for " + ident.app);
         }
@@ -120,68 +111,6 @@ public class CheckDone implements Runnable{
             }
             ident.ts = Util.getNextWholeTs(ident.ts, ident.period);
         }
-    }
-    
-    public Map<String, Set<String>> findInstancesByCmdbApps(String topic, List<String> cmdbApps)
-            throws UnsupportedEncodingException, JSONException {
-        if (cmdbApps.size() == 0) {
-            return null;
-        }
-        //HTTP get the json
-        StringBuilder catalogURLBuild = new StringBuilder();
-        catalogURLBuild.append(getPaaSInstanceURLPerfix);
-        for (String app : cmdbApps) {
-            catalogURLBuild.append(app).append(",");
-        }
-        catalogURLBuild.deleteCharAt(catalogURLBuild.length() - 1);
-        String response = httpClient.getResponseText(catalogURLBuild.toString());
-        if (response == null) {
-            LOG.error("response is null.");
-            return null;
-        }
-        String jsonStr = java.net.URLDecoder.decode(response, "utf-8");
-        JSONObject rootObject = new JSONObject(jsonStr);
-        JSONArray catalogArray = rootObject.getJSONArray("catalog");
-        if (catalogArray.length() == 0) {
-            LOG.warn("Can not get any catalog by url " + catalogURLBuild.toString());
-        }
-        Map<String, Set<String>> hostToInstances = new HashMap<String, Set<String>>();
-        for (int i = 0; i < catalogArray.length(); i++) {
-            JSONObject layoutObject = catalogArray.getJSONObject(i);
-            String app = (String)layoutObject.get("app");
-            JSONArray layoutArray = layoutObject.getJSONArray("layout");
-            if (layoutArray.length() == 0) {
-                LOG.warn("Can not get any layout by app " + app);
-            }
-            for (int j = 0; j < layoutArray.length(); j++) {
-                JSONObject hostIdObject = layoutArray.getJSONObject(j);
-                String ip = (String) hostIdObject.get("ip");
-                InetAddress host;
-                try {
-                    host = InetAddress.getByName(ip);
-                } catch (UnknownHostException e) {
-                    LOG.error("Receive a unknown host " + ip, e);
-                    continue;
-                }
-                String agentHost = host.getHostName();
-                addTopicToHost(agentHost, topic);
-                
-                Set<String> instances;
-                if ((instances = hostToInstances.get(agentHost)) == null) {
-                    instances = new HashSet<String>();
-                    hostToInstances.put(agentHost, instances);
-                }
-                JSONArray idArray = hostIdObject.getJSONArray("id");
-                if (idArray.length() == 0) {
-                    LOG.warn("Can not get any instance by host " + agentHost);
-                }
-                for (int k = 0; k < idArray.length(); k++) {
-                    LOG.debug("PaaS catalog app: " + app + ", ip: " + ip + ", id: " + idArray.getString(k));
-                    instances.add(idArray.getString(k));
-                }
-            }
-        }
-        return hostToInstances;
     }
 
     /**
