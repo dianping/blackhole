fa4fb8709a82104721ca1c68d02263ef32e02993
LantaoJin <lantao.jin@dianping.com>
2015-07-15 14:48:30 +0800

    print topic when ProducerRequest throws a BufferUnderflowException for reading

--- blackhole-common/src/main/java/com/dp/blackhole/protocol/data/ProduceRequest.java
+++ blackhole-common/src/main/java/com/dp/blackhole/protocol/data/ProduceRequest.java
@@ -2,12 +2,16 @@ package com.dp.blackhole.protocol.data;
 
 import java.nio.ByteBuffer;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.dp.blackhole.network.GenUtil;
 import com.dp.blackhole.network.NonDelegationTypedWrappable;
 import com.dp.blackhole.storage.ByteBufferMessageSet;
 import com.dp.blackhole.storage.MessageSet;
 
 public class ProduceRequest extends NonDelegationTypedWrappable {
+    private static final Log LOG = LogFactory.getLog(ProduceRequest.class);
     public String topic;
     public String partitionId;
     ByteBufferMessageSet messages;
@@ -32,7 +36,12 @@ public class ProduceRequest extends NonDelegationTypedWrappable {
     @Override
     public void read(ByteBuffer buffer) {
         topic = GenUtil.readString(buffer);
-        partitionId = GenUtil.readString(buffer);
+        try {
+            partitionId = GenUtil.readString(buffer);
+        } catch (RuntimeException e) {
+            LOG.fatal(topic, e);
+            throw e;
+        }
         messages = new ByteBufferMessageSet(buffer.slice());
     }
 
